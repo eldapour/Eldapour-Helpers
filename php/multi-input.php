c6bb49e607e2ee429cb0005210fa9035dcd317e7
Abdallah Mahmoud <Abdalluhmahmoud55@gmail.com>
2023-04-18 17:20:04 +0200

    18-4-2023

--- php/multi-input.php
+++ php/multi-input.php
@@ -1,29 +1,35 @@
-<label class="control-label">itemes</label>
-<div class="col-4">
-    <div class="form-group itemKeys">
-        <label class="control-label">Key</label>
-        <input type="text" name="items[0][key]" class="form-control InputKey">
-    </div>
-</div>
-<div class="col-4">
-    <div class="form-group itemItems">
-        <label class="control-label">Value</label>
-        <input type="text" name="items[0][value]" class="form-control InputItem">
-    </div>
-</div>
-<div class="col-4">
-    <div class="form-group ButtonItems">
-        <button type="button" class="btn btn-sm btn-primary MoreItem">More items</button>
+<div class="row">
+    <label class="control-label">element</label>
+    <div class="col-4">
+        <div class="form-group itemItems">
+            <input type="text" name="items[]" class="form-control InputItem InputItemExtra" value="">
+        </div>
     </div>
 </div>
 <script>
-    var i = 0;
-    $('.MoreItem').on('click', function () {
+    $(document).on('click', '.delItem', function () {
+        var Item = $('.InputItemExtra').last();
+        if (Item.val() == '') {
+            Item.fadeOut();
+            Item.remove();
+            $('.Issue').removeClass('badge-danger').addClass('badge-success');
+            $('.Issue').html('The element deleted');
+            setTimeout(function () {
+                $('.Issue').html('');
+            }, 3000)
+        } else {
+            $('.Issue').html('The element must be empty');
+            setTimeout(function () {
+                $('.Issue').html('');
+            }, 3000)
+
+        }
+    })
+
+    $(document).on('click', '.MoreItem', function () {
         var Item = $('.InputItemExtra').last();
         if (Item.val() !== '') {
-            ++i;
-            $('.itemKeys').append('<label class="control-label">Key</label><input type="text" name="items[' + i + '][key]" class="form-control InputKeyExtra">')
-            $('.itemItems').append(' <label class="control-label">Value</label><input type="text" name="items[' + i + '][value]" class="form-control InputItemExtra">')
+            $('.itemItems').append('<input type="text" name="items[]" class="form-control InputItemExtra mt-3">')
         }
     })
 </script>
